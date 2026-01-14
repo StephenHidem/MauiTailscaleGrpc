1075502704bdfebfc179f6cfe1ca40e880d8b8a6
Stephen Hidem <StephenHidem@outlook.com>
2026-01-13 20:16:18 -0600

    Added ByteArrayToStringConverter. Moved StreamToImageSourceConverter into App.xaml. Updated UnknownDevice UI.

---
+++ AntPlusMauiClient/Converters/ByteArrayToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace AntPlusMauiClient.Converters
+{
+    public class ByteArrayToStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is byte[] bytes)
+            {
+                return BitConverter.ToString(bytes);
+            }
+            return string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AntPlusMauiClient/ViewModels/UnknownDeviceViewModel.cs
+++ AntPlusMauiClient/ViewModels/UnknownDeviceViewModel.cs
@@ -5,28 +5,11 @@ namespace AntPlusMauiClient.ViewModels
 {
     public class UnknownDeviceViewModel
     {
-        private UnknownDevice _unknownDevice;
-        public ObservableCollection<string> DataPages { get; private set; } = new ObservableCollection<string>();
+        public ObservableCollection<byte[]> DataPages { get; private set; }
 
         public UnknownDeviceViewModel(UnknownDevice unknownDevice)
         {
-            _unknownDevice = unknownDevice;
-            foreach (byte[] page in _unknownDevice.DataPages)
-            {
-                DataPages.Add(BitConverter.ToString(page));
-            }
-
-            _unknownDevice.DataPages.CollectionChanged += (s, e) =>
-            {
-                MainThread.BeginInvokeOnMainThread(() =>
-                {
-                    DataPages.Clear();
-                    foreach (byte[] page in _unknownDevice.DataPages)
-                    {
-                        DataPages.Add(BitConverter.ToString(page));
-                    }
-                });
-            };
+            DataPages = unknownDevice.DataPages;
         }
     }
 }
